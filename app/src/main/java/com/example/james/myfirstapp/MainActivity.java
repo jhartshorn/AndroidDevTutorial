bb2fadf25f44393f4a5321247df75b18e3ffda3d
jhartshorn <jhartshorn@gmail.com>
2014-11-14 22:05:24 +0000

    Added activities for Search and Settings

--- app/src/main/java/com/example/james/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/james/myfirstapp/MainActivity.java
@@ -35,20 +35,34 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
+    private void openSearch() {
+        Intent intent = new Intent(this, SearchActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.search_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
+    private void openSettings() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.settings_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
@@ -58,4 +72,6 @@ public class MainActivity extends Activity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
+
 }
